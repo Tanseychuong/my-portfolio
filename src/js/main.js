96aab2039dba5ab77e7586cf810b2299995783f3
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-07-24 19:52:40 +0000

    main js added

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import { initNavigation } from "./navigation.js";
+import { initAnimations } from "./animations.js";
+import { initTyping } from "./typing.js";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    console.log("Portfolio initialized");
+
+
+    initNavigation();
+
+    initAnimations();
+
+    initTyping();
+
+
+});
