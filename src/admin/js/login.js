91418c0539fca1a239b7f437c709c919991adee2
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-08-01 22:08:25 +0000

    Api route added for login.js

--- src/admin/js/login.js
+++ src/admin/js/login.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", async (e) => {
     try {
 
         const response = await fetch(
-            "http://localhost:5000/api/auth/login",
+            `${API_BASE}/api/auth/login`,
             {
                 method: "POST",
                 headers: {
