ca40e3766dd847810cf8eabab21f996753e1cbc4
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-08-02 23:06:17 +0000

    /src/config/.js refactored

--- src/js/config.js
+++ src/js/config.js
@@ -11,4 +11,4 @@ const API_BASE = (
     window.location.hostname === "127.0.0.1"
 )
     ? "http://localhost:5000"
-    : "https://REPLACE-WITH-YOUR-BACKEND-URL.vercel.app";
+    : "https://my-portfolio-ktpf.vercel.app";
